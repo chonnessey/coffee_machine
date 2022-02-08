689da7ff39a46adcfe00c96eca310f331ba69b7b
Adrian Camacho <adriancamacho18@gmail.com>
2022-02-07 22:20:03 -0800

    add finished script for coffee machine

--- main.py
+++ main.py
@@ -63,6 +63,12 @@ def is_transaction_successful(money_received, drink_cost):
     print("Sorry this is not enough money :(")
     return False
 
+def make_coffee(drink_name, order_ingredients):
+  """Deduct the requried ingredients from resources"""
+  for item in order_ingredients:
+    resources[item] -= order_ingredients[item]
+  print(f"Here is your {drink_name} ☕️")
+
 is_on = True
 
 while is_on:
@@ -78,5 +84,6 @@ while is_on:
     drink = MENU[choice]
     if is_resource_sufficient(drink["ingredients"]):
       payment = process_coins()
-      is_transaction_successful(payment, drink["cost"])
+      if is_transaction_successful(payment, drink["cost"]):
+        make_coffee(choice, drink["ingredients"])
 
